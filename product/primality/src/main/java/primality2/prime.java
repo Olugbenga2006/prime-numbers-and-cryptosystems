f96e84e30eda2d8b359f35f8cddbcb0f08915cc2
Olugbenga2006 <157271915+Olugbenga2006@users.noreply.github.com>
2024-11-01 14:10:26 +0000

    ptime number testing efficiency was improved by checking up to the square root of the prime. testing time reduced exponentially

--- product/primality/src/main/java/primality2/prime.java
+++ product/primality/src/main/java/primality2/prime.java
@@ -1,15 +1,15 @@
 package primality2;
 
 public class prime {
-  public boolean isprime(long n) {
-    if (n<=1) {
-      return false;}
-    for (long i=2; i<n; i++) {
-      if (n%i==0) {
-        return false;
+    public boolean isprime(long n) {
+        if (n <= 1) {
+            return false;
         }
-      }
-    return true;
+        for (long i = 2; i * i <= n; i++) { // Check up to the square root of n
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
-    
-  }
+}
