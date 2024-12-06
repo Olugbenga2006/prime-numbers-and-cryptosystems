4ecab46f32393166513c6e666a8c94c0269cb3ec
Olugbenga2006 <157271915+Olugbenga2006@users.noreply.github.com>
2024-12-06 16:06:07 +0000

    added miller-rabin test for primality and succesfully tested 300 digit primes

--- product/primality/src/main/java/primality2/LargerPrimes.java
+++ product/primality/src/main/java/primality2/LargerPrimes.java
@@ -1,26 +1,96 @@
 package primality2;
 
 import java.math.BigInteger;
+import java.io.*;
+import java.math.*;
 
 public class LargerPrimes {
-    public boolean isPrime(BigInteger n) {
-        if (n.compareTo(BigInteger.ONE) <= 0) {
-            return false;
-        }
-        if (n.equals(BigInteger.TWO)) {
-            return true; // 2 is prime
-        }
-        if (n.mod(BigInteger.TWO).equals(BigInteger.ZERO)) {
-            return false; // Exclude even numbers greater than 2
-        }
+  // returns (x^y) % p
+  static BigInteger modpower(BigInteger x, BigInteger y, BigInteger p) {
 
-        // Check for factors from 3 up to the square root of n (incrementing by 2)
-        BigInteger sqrtN = n.sqrt();
-        for (BigInteger i = BigInteger.valueOf(3); i.compareTo(sqrtN) <= 0; i = i.add(BigInteger.TWO)) {
-            if (n.mod(i).equals(BigInteger.ZERO)) {
-                return false; // n is divisible by i
-            }
-        }
-        return true; // n is prime
+    BigInteger res = BigInteger.ONE;
+
+    // x cannot be larger than p
+    x = x.mod(p);
+
+    while (y.compareTo(BigInteger.ZERO) > 0) {
+      // If y is odd, multiply x with result
+      if (y.and(BigInteger.ONE).equals(BigInteger.ONE)) // y%2 == 1
+        res = (res.multiply(x)).mod(p);
+
+      // y must be even now
+      y = y.shiftRight(1); // y = y/2
+      x = (x.multiply(x)).mod(p);
     }
+
+    return res;
+  }
+
+  // This function is called for all k 
+  // It returns false if n is composite and returns true if n is probably prime.
+  // d is an odd number such that (2^r)*d = n-1 for some r >= 1
+  static boolean millerTest(BigInteger d, BigInteger n) {
+
+    // Pick a random number in [2..n-2]
+    // Corner cases make sure that n > 4
+    BigInteger a = BigInteger.valueOf(2)
+        .add(new BigInteger(n.subtract(BigInteger.valueOf(4)).bitLength(), new java.util.Random()).mod(n.subtract(BigInteger.valueOf(4))));
+
+    // Compute a^d % n
+    BigInteger x = modpower(a, d, n);
+
+    if (x.equals(BigInteger.ONE) || x.equals(n.subtract(BigInteger.ONE)))
+      return true;
+
+    // Keep squaring x while one of the following doesn't happen
+    // d does not reach n-1
+    // (x^2) % n is not 1
+    // (x^2) % n is not n-1
+    while (!d.equals(n.subtract(BigInteger.ONE))) {
+      x = (x.multiply(x)).mod(n);
+      d = d.shiftLeft(1); // d *= 2
+
+      if (x.equals(BigInteger.ONE)) {
+        return false;
+      }
+      if (x.equals(n.subtract(BigInteger.ONE))) {
+        return true;
+      }
+    }
+
+    // Return composite
+    return false;
+  }
+
+  // It returns false if n is composite 
+  // and returns true if n is probably prime. 
+  // k is an input parameter that determines accuracy level.
+  // Higher value of k indicates more accuracy.
+  static boolean isPrime(BigInteger n, int k) {
+
+    // Corner cases
+    if (n.compareTo(BigInteger.ONE) <= 0 || n.equals(BigInteger.valueOf(4))) {
+      return false;
+    }
+    if (n.compareTo(BigInteger.valueOf(3)) <= 0) {
+      return true;
+    }
+
+    // Find r such that n = 2^d * r + 1 
+    // for some r >= 1
+    BigInteger d = n.subtract(BigInteger.ONE);
+    
+    while (d.mod(BigInteger.TWO).equals(BigInteger.ZERO)) {
+      d = d.divide(BigInteger.TWO);
+    }
+
+
+    // Iterate given number of 'k' times
+    for (int i = 0; i < k; i++)
+      if (!millerTest(d, n))
+        return false;
+
+    return true;
+  }
+
 }
--- product/primality/src/test/java/primality2/isitprime.java
+++ product/primality/src/test/java/primality2/isitprime.java
@@ -1,83 +1,91 @@
 package primality2;
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigInteger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class isitprime {
-  private prime primeChecker;
+  private LargerPrimes primeChecker;
 
   @BeforeEach
   public void setUp() {
-      primeChecker = new prime();
+    primeChecker = new LargerPrimes();
   }
 
   @Test
   public void testIsPrime_WithNegativeNumber() {
-      // Test negative numbers
-      assertFalse(primeChecker.isprime(-5), "Negative numbers should not be prime.");
+    // Test negative numbers
+    BigInteger largePrime = new BigInteger("-5");
+    assertFalse(primeChecker.isPrime(largePrime,2), "Negative numbers should not be prime.");
   }
 
   @Test
   public void testIsPrime_WithZero() {
-      // Test zero
-      assertFalse(primeChecker.isprime(0), "Zero should not be prime.");
+    // Test zero
+    BigInteger largePrime = new BigInteger("0");
+    assertFalse(primeChecker.isPrime(largePrime,2), "Zero should not be prime.");
   }
 
   @Test
   public void testIsPrime_WithOne() {
-      // Test one
-      assertFalse(primeChecker.isprime(1), "One should not be prime.");
+    // Test one
+    BigInteger LargePrime = new BigInteger("1");
+    assertFalse(primeChecker.isPrime(LargePrime,2), "One should not be prime.");
   }
 
   @Test
   public void testIsPrime_WithTwo() {
-      // Test two (the smallest prime number)
-      assertTrue(primeChecker.isprime(2), "Two should be prime.");
+    // Test two (the smallest prime number)
+    BigInteger LargePrime = new BigInteger("2");
+    assertTrue(primeChecker.isPrime(LargePrime,2), "Two should be prime.");
   }
 
   @Test
   public void testIsPrime_WithThree() {
-      // Test three
-      assertTrue(primeChecker.isprime(3), "Three should be prime.");
+    // Test three
+    BigInteger LargePrime = new BigInteger("3");
+    assertTrue(primeChecker.isPrime(LargePrime,2), "Three should be prime.");
   }
 
   @Test
   public void testIsPrime_WithFour() {
-      // Test four
-      assertFalse(primeChecker.isprime(4), "Four should not be prime.");
+    // Test four
+    BigInteger LargePrime = new BigInteger("4");
+    assertFalse(primeChecker.isPrime(LargePrime,2), "Four should not be prime.");
   }
   @Test
   public void testIsPrime_WithPrimeNumber() {
-      // Test a known prime number
-      assertTrue(primeChecker.isprime(17), "Seventeen should be prime.");
+    // Test a known prime number
+    BigInteger LargePrime = new BigInteger("17");
+    assertTrue(primeChecker.isPrime(LargePrime,2), "Seventeen should be prime.");
   }
 
   @Test
   public void testIsPrime_WithCompositeNumber() {
-      // Test a known composite number
-      assertFalse(primeChecker.isprime(10), "Ten should not be prime.");
+    // Test a known composite number
+    BigInteger LargePrime = new BigInteger("10");
+    assertFalse(primeChecker.isPrime(LargePrime,2), "Ten should not be prime.");
   }
 
   @Test
   public void testIsPrime_WithLargePrimeNumber() {
-      // Test a large prime number
-      assertTrue(primeChecker.isprime(7919), "Seven thousand nine hundred nineteen should be prime.");
+    // Test a large prime number
+    BigInteger LargePrime = new BigInteger("7919");
+    assertTrue(primeChecker.isPrime(LargePrime,5), "Seven thousand nine hundred nineteen should be prime.");
   }
   @Test
   public void testIsPrime_WithLargeCompositeNumber() {
-      // Test a large composite number
-      assertFalse(primeChecker.isprime(10000), "Ten thousand should not be prime.");
+    // Test a large composite number
+    BigInteger LargePrime = new BigInteger("10000");
+    assertFalse(primeChecker.isPrime(LargePrime,2), "Ten thousand should not be prime.");
   }
   @Test
   public void testIsPrime_WithLargerPrimeNumber() {
-      // Test a  larger prime number
-      assertTrue(primeChecker.isprime(1053569021), "should be prime");
+    // Test a  larger prime number
+    BigInteger LargePrime = new BigInteger("1053569021");
+    assertTrue(primeChecker.isPrime(LargePrime,15), "should be prime");
   }
-  @Test
-  public void testIsPrime_WithevenLargerPrimeNumber() {
-      // Test the very large prime number
-      assertTrue(primeChecker.isprime(35663926699L), "The number 35663926699 should be prime.");
-  }
+  
 
 }
